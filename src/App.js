9e819affa42b58490a402f724dd0b49660af8ea2
Mark Roenfeldt <1187productions@gmail.com>
2020-11-23 21:10:10 -0700

    Progress on Filter feature

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import './App.css';
 import Table from './Table';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       restaurantArray: []
+    }
+    this.selectedFilter = new Set()
+  }
+
+  toggleFilter = label => {
+    if (this.selectedFilter.has(label)) {
+      this.selectedFilter.delete(label);
+    } else {
+      this.selectedFilter.add(label);
     }
   }
 
@@ -16,14 +26,17 @@ class App extends Component {
     fetch(apiURL, { headers: {"Authorization": "Api-Key q3MNxtfep8Gt"}})
     .then((response) => response.json())
     .then(restaurantArray => this.setState({ restaurantArray }))
+
+    const [filter, setFilter] = useState()
+    const handleClick = () => setFilter(filter)
   }
 
   render() {
       return (
         <div className='App'>
           <h1>Restaurant Respository</h1>
-          <Header />  
-          <Table restaurantArray={ this.state.restaurantArray.sort((a,b) => (a.name > b.name) ? 1 : -1) } />
+          <Header checkStatus={setFilter}/>  
+          <Table restaurantArray={ this.state.restaurantArray.sort((a,b) => (a.name > b.name) ? 1 : -1) } check={filter}/>
         </div>
       )
   }
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Header() {
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { isChecked: false}
+    }
     
+    toggleFilterChange = () => {
+        const { handleFilterChange, label } = this.props
+        this.setState(({ isChecked }) => {
+            return { isChecked: !isChecked }
+        })
 
-    return (
-        <div className='header-container'>
-            <button onClick='filter'>Filter</button> 
-            <input type='text' placeholder='Search...'></input>
-        </div>
-    )
+        handleFilterChange(label)
+    }
+
+    render() {
+        const { label } = this.props
+        const { isChecked } = this.state
+
+        return (
+            <div className='header-container'>
+                <div className='filter-container'>
+                    <form>
+                        <h2>Filter</h2>
+                        <label for="allFilter">
+                            <input type="checkbox" value={label} checked={isChecked} />    
+                        </label><br />
+                        <label for="azFilter">
+                            <input type="checkbox" value={label} checked={isChecked} />    
+                        </label><br />
+                        {/* <input type="checkbox" onChange={this.filter("AZ")} />
+                        <label for="allFilter">AZ</label><br />
+                        <input type="checkbox" onChange={this.filter("CA")} />
+                        <label for="allFilter">CA</label><br />
+                        <input type="checkbox" onChange={this.filter("CO")} />
+                        <label for="allFilter">CO</label><br />
+                        <input type="checkbox" onChange={this.filter("FL")} />
+                        <label for="allFilter">FL</label><br />
+                        <input type="checkbox" onChange={this.filter("HI")} />
+                        <label for="allFilter">HI</label><br />
+                        <input type="checkbox" onChange={this.filter("MA")} />
+                        <label for="allFilter">MA</label><br />
+                        <input type="checkbox" onChange={this.filter("MD")} />
+                        <label for="allFilter">MD</label><br />
+                        <input type="checkbox" onChange={this.filter("MN")} />
+                        <label for="allFilter">MN</label><br />
+                        <input type="checkbox" onChange={this.filter("NC")} />
+                        <label for="allFilter">NC</label><br />
+                        <input type="checkbox" onChange={this.filter("NJ")} />
+                        <label for="allFilter">NJ</label><br />
+                        <input type="checkbox" onChange={this.filter("NV")} />
+                        <label for="allFilter">NV</label><br />
+                        <input type="checkbox" onChange={this.filter("NY")} />
+                        <label for="allFilter">NY</label><br />
+                        <input type="checkbox" onChange={this.filter("OR")} />
+                        <label for="allFilter">OR</label><br />
+                        <input type="checkbox" onChange={this.filter("TN")} />
+                        <label for="allFilter">TN</label><br />
+                        <input type="checkbox" onChange={this.filter("TX")} />
+                        <label for="allFilter">TX</label><br />
+                        <input type="checkbox" onChange={this.filter("VA")} />
+                        <label for="allFilter">VA</label><br />
+                        <input type="checkbox" onChange={this.wfilter("WV")} />
+                        <label for="allFilter">WV</label><br /> */}
+                    </form>
+                </div>
+                <div className='search-container'>
+                    <input type='text' placeholder='Search...'></input>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Header
